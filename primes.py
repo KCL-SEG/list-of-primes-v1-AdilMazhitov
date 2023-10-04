fc7723694dff9b35e77b7f9c6d0aecc663b3bcaf
Adil Mazhitov <k21117945@kcl.ac.uk>
2023-10-05 02:23:43 +0600

    Add the function of creating prime list

--- primes.py
+++ primes.py
@@ -3,4 +3,18 @@
 
 def primes(number_of_primes):
     list = []
-    return list
+    primeNumber = 2
+    while (len(list) < int(number_of_primes)):
+        counter = 0
+        for i in range(1, primeNumber+1):
+            if primeNumber % i == 0:
+                counter += 1
+        
+        if (counter == 2):
+            list.append(primeNumber)
+
+        primeNumber += 1 
+
+    print(list)
+
+primes(10)
